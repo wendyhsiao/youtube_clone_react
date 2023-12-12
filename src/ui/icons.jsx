961de2fbaa905b0118065c629817d264f5ffad91
wendy.hsiao <wendy19940331@gmail.com>
2023-12-12 15:34:38 +0800

    add icons

---
+++ src/ui/icons.jsx
@@ -0,0 +1,31 @@
+import { ReactComponent as Logo } from '../assets/icons/logo.svg';
+import { ReactComponent as Like } from '../assets/icons/like.svg';
+import { ReactComponent as LikeFill } from '../assets/icons/like-fill.svg';
+import { ReactComponent as Dislike } from '../assets/icons/dislike.svg';
+import { ReactComponent as Share } from '../assets/icons/share.svg';
+import { ReactComponent as Save } from '../assets/icons/save.svg';
+import { ReactComponent as Report } from '../assets/icons/report.svg';
+import { ReactComponent as Down } from '../assets/icons/down.svg';
+import { ReactComponent as UpDown } from '../assets/icons/updown.svg';
+import { ReactComponent as More } from '../assets/icons/more.svg';
+import { ReactComponent as Search } from '../assets/icons/search.svg';
+import { ReactComponent as Close } from '../assets/icons/close.svg';
+import { ReactComponent as Comment } from '../assets/icons/comment.svg';
+import { ReactComponent as SpecBtn } from '../assets/icons/spec-btn.svg';
+
+export default {
+  Logo,
+  Like,
+  LikeFill,
+  Dislike,
+  Share,
+  Save,
+  Report,
+  Down,
+  UpDown,
+  More,
+  Search,
+  Close,
+  Comment,
+  SpecBtn,
+};
